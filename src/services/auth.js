640bbc88ca53656f054b9c37c5eaed6bcb2b55e5
varun tej reddy <varuntejreddy@varuns-MacBook-Air.local>
2026-02-16 10:09:31 +0530

    Updated auth to match frontend fields

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,3 @@
-// src/services/auth.js
 import axios from "axios";
 import { API_BASE_URL } from "../config";
 
@@ -17,12 +16,14 @@ export function logout() {
   localStorage.removeItem(CURRENT_USER);
 }
 
+/* ================= REGISTER ================= */
 export async function register(userInfo) {
   const res = await axios.post(`${API_BASE_URL}/auth/register`, userInfo);
   setCurrentUser(res.data.user);
   return res.data;
 }
 
+/* ================= LOGIN ================= */
 export async function login(credentials) {
   const res = await axios.post(`${API_BASE_URL}/auth/login`, credentials);
   setCurrentUser(res.data.user);
